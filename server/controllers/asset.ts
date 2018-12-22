168fdc30f2048d5c4ecd2d90436fce0e14ac8ffb
ilay.pilo@gmail.com <ilay.pilo@gmail.com>
2018-12-23 00:50:08 +0200

    added api guard

--- server/controllers/asset.ts
+++ server/controllers/asset.ts
@@ -6,17 +6,4 @@ import BaseCtrl from './base';
 
 export default class AssetCtrl extends BaseCtrl {
   model = asset;
-
-
-  // login = (req, res) => {
-  //   this.model.findOne({ email: req.body.email }, (err, user) => {
-  //     if (!user) { return res.sendStatus(403); }
-  //     user.comparePassword(req.body.password, (error, isMatch) => {
-  //       if (!isMatch) { return res.sendStatus(403); }
-  //       const token = jwt.sign({ user: user }, process.env.SECRET_TOKEN); // , { expiresIn: 10 } seconds
-  //       res.status(200).json({ token: token });
-  //     });
-  //   });
-  // }
-
 }
