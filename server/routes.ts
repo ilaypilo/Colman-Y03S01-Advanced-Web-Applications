071817f77de72689905724a321ad4f3adac9b077
ilay.pilo@gmail.com <ilay.pilo@gmail.com>
2018-12-23 19:13:19 +0200

    separate token parsing from access guard

--- server/routes.ts
+++ server/routes.ts
@@ -4,54 +4,46 @@ import * as jwt from 'jsonwebtoken';
 import UserCtrl from './controllers/user';
 import AssetCtrl from './controllers/asset';
 
-let adminGuard = (req, res, next) => {
-  if (!req.headers.authorization) {
+let checkToken = (req, res, next) => {
+  let token = req.headers.authorization;
+  if (token.startsWith('Bearer ')) {
+    // Remove Bearer from string
+    token = token.slice(7, token.length);
+  }
+  if (!token) {
     res.send(401);
     return;
   }
-  jwt.verify(req.headers.authorization, process.env.SECRET_TOKEN, function(err, decoded) {
-    if (decoded.user.role != "admin") {
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    if (err) {
       res.send(401);
       return;
+    } else {
+      req.decoded = decoded;
+      next();
     }
-    return next();
   });
+};
+
+let adminGuard = (req, res, next) => {
+  if (req.decoded.user.role === "admin") {
+    return next();
+  }
+  res.send(401);
 }
 
 let loginGuard = (req, res, next) => {
-  if (!req.headers.authorization){
-    res.send(401);
-    return;
-  }
-  jwt.verify(req.headers.authorization, process.env.SECRET_TOKEN, function(err, decoded) {
-    if (err) {
-      res.send(401);
-      return;
-    }
-    if (!decoded.user.role) {
-      res.send(401);
-      return;
-    }
+  if (req.decoded.user.role) {
     return next();
-  });
+  }
+  res.send(401);
 }
 
 let selfGuard = (req, res, next) => {
-  if (!req.headers.authorization){
-    res.send(401);
-    return;
-  }
-  jwt.verify(req.headers.authorization, process.env.SECRET_TOKEN, function(err, decoded) {
-    if (err) {
-      res.send(401);
-      return;
-    }
-    if (req.path.split('/').pop() != decoded.user._id && decoded.user.role != "admin") {
-      res.send(401);
-      return;
-    }
+  if (req.path.split('/').pop() === req.decoded.user._id || req.decoded.user.role === "admin") {
     return next();
-  });
+  }
+  res.send(401);
 }
 
 export default function setRoutes(app) {
@@ -64,14 +56,14 @@ export default function setRoutes(app) {
   // Users
   router.route('/login').post(userCtrl.login);
   router.route('/user').post(userCtrl.insert);
-  router.route('/users').all(adminGuard).get(userCtrl.getAll);
-  router.route('/user/:id').all(selfGuard).get(userCtrl.get);
-  router.route('/user/:id').all(selfGuard).put(userCtrl.update);
-  router.route('/user/:id').all(adminGuard).delete(userCtrl.delete);
+  router.route('/users').all(checkToken).all(adminGuard).get(userCtrl.getAll);
+  router.route('/user/:id').all(checkToken).all(selfGuard).get(userCtrl.get);
+  router.route('/user/:id').all(checkToken).all(selfGuard).put(userCtrl.update);
+  router.route('/user/:id').all(checkToken).all(adminGuard).delete(userCtrl.delete);
 
   // Assets
-  router.route('/assets').all(loginGuard).get(assetCtrl.getAll);
-  router.route('/asset/:id').all(loginGuard).get(assetCtrl.get);
+  router.route('/assets').all(checkToken).all(loginGuard).get(assetCtrl.getAll);
+  router.route('/asset/:id').all(checkToken).all(loginGuard).get(assetCtrl.get);
 
   // Apply the routes to our application with the prefix /api
   app.use('/api', router);
