32a5e7caba4ceaa1e44c02ad50de12af61e47bc9
ilay.pilo@gmail.com <ilay.pilo@gmail.com>
2019-02-15 12:03:46 +0200

    added asset filter

--- client/app/pages/assets/assets.component.ts
+++ client/app/pages/assets/assets.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../../services/auth.service';
 import { AssetService } from '../../services/asset.service';
 import { Asset } from '../../shared/models/asset.model';
 import { MatDialog, MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
+import { FormControl } from '@angular/forms';
 
 
 @Component({
@@ -18,11 +19,28 @@ export class AssetsComponent implements OnInit {
   title = 'Registered Assets';
   assets: Asset[] = [];
   isLoading = true;
-  displayedColumns = ['city', 'neighborhood', 'street', 'price', 'rooms', 'floor', 'home_type' , 'action'];
+  displayedColumns = ['city', 'neighborhood', 'street', 'price', 'Rooms_text', 'Floor_text', 'HomeTypeID_text' , 'action'];
   dataSource: any;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  
+
+  filterValues = {
+    city: '',
+    neighborhood: '',
+    street: '',
+    price: '',
+    Rooms_text: '',
+    Floor_text: '',
+    HomeTypeID_text: ''
+  };
+  cityFilter = new FormControl('');
+  neighborhoodFilter = new FormControl('');
+  streetFilter = new FormControl('');
+  priceFilter = new FormControl('');
+  roomsFilter = new FormControl('');
+  floorFilter = new FormControl('');
+  homeTypeFilter = new FormControl('');
+
   constructor(
     public auth: AuthService,
     public toast: ToastComponent,
@@ -33,6 +51,72 @@ export class AssetsComponent implements OnInit {
 
   ngOnInit() {
     this.getAssets();
+
+    this.cityFilter.valueChanges
+      .subscribe(
+        value => {
+          this.filterValues.city = value;
+          this.dataSource.filter = JSON.stringify(this.filterValues);
+        }
+      )
+      this.neighborhoodFilter.valueChanges
+      .subscribe(
+        value => {
+          this.filterValues.neighborhood = value;
+          this.dataSource.filter = JSON.stringify(this.filterValues);
+        }
+      )
+      this.streetFilter.valueChanges
+      .subscribe(
+        value => {
+          this.filterValues.street = value;
+          this.dataSource.filter = JSON.stringify(this.filterValues);
+        }
+      )
+      this.priceFilter.valueChanges
+      .subscribe(
+        value => {
+          this.filterValues.price = value;
+          this.dataSource.filter = JSON.stringify(this.filterValues);
+        }
+      )
+      this.roomsFilter.valueChanges
+      .subscribe(
+        value => {
+          this.filterValues.Rooms_text = value;
+          this.dataSource.filter = JSON.stringify(this.filterValues);
+        }
+      )
+      this.floorFilter.valueChanges
+      .subscribe(
+        value => {
+          this.filterValues.Floor_text = value;
+          this.dataSource.filter = JSON.stringify(this.filterValues);
+        }
+      )
+      this.homeTypeFilter.valueChanges
+      .subscribe(
+        value => {
+          this.filterValues.HomeTypeID_text = value;
+          this.dataSource.filter = JSON.stringify(this.filterValues);
+        }
+      )
+  }
+
+  createFilter(): (data: any, filter: string) => boolean {
+    let filterFunction = function(data, filter): boolean {
+      let searchTerms = JSON.parse(filter);
+      let flag = true;
+      Object.keys(searchTerms).forEach(function(key) {
+        if (searchTerms[key] !== '') {
+          if (!data[key] || data[key].toString().indexOf(searchTerms[key]) === -1) {
+            flag = false;
+          }
+        } 
+      })
+      return flag;
+    }
+    return filterFunction;
   }
 
   getAssets() {
@@ -42,6 +126,7 @@ export class AssetsComponent implements OnInit {
         this.dataSource = new MatTableDataSource<Asset>(this.assets);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
+        this.dataSource.filterPredicate = this.createFilter();
       },
       error => console.log(error),
       () => this.isLoading = false
